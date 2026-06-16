9f86c4ba99aeca7456594f9905cdc0b37fc52008
Kims0ngmin <dae8707@naver.com>
2026-06-16 10:39:04 +0900

    Configure GitHub Pages deployment

--- src/lib/useScrollReveal.ts
+++ src/lib/useScrollReveal.ts
@@ -1,4 +1,5 @@
-import { useEffect, RefObject } from 'react';
+import { useEffect } from 'react';
+import type { RefObject } from 'react';
 
 export function useScrollReveal(ref: RefObject<HTMLElement | null>, options?: IntersectionObserverInit) {
   useEffect(() => {
